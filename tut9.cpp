2c0c9a0d118356a1bf3eeacbf6b4f082eada4525
git-harshh <harsh.r.4221@gmail.com>
2025-06-19 12:01:34 +0530

    commit

--- tut9.cpp
+++ tut9.cpp
@@ -2,7 +2,21 @@
 using namespace std;
 
 int main(){
-   cout<<"this is tutorial 9";
+
+    //    cout<<"this is tutorial 9";
+    int age;
+    cout<<"tell me your age";
+    cin>>age;
+    if(age<18)
+    cout<<"you can't come to the party"<<endl;
+    else if (age==18)
+    {
+        cout<<"You are a kid, and u will et a permit to the party"; 
+    } 
+    else {
+        cout<<"you can come to the party";
+    }
+
     
     return 0;
 };
